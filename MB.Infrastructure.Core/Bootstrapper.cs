c84b5f4bf95b91a8c728b8f89f9a482eaa203d56
Alireza Pakravan <66172389+alirezapaakraavaan@users.noreply.github.com>
2021-03-29 12:09:10 +0430

    List articles implemented

--- MB.Infrastructure.Core/Bootstrapper.cs
+++ MB.Infrastructure.Core/Bootstrapper.cs
@@ -1,5 +1,7 @@
 ﻿using MB.Application;
+using MB.Application.Contracts.Article;
 using MB.Application.Contracts.ArticleCategory;
+using MB.Domain.ArticleAgg;
 using MB.Domain.ArticleCategoryAgg;
 using MB.Domain.ArticleCategoryAgg.Services;
 using MB.Infrastructure.EFCore;
@@ -16,6 +18,8 @@ namespace MB.Infrastructure.Core
             services.AddTransient<IArticleCategoryApplication, ArticleCategoryApplication>();
             services.AddTransient<IArticleCategoryRepository, ArticleCategoryRepository>();
             services.AddTransient<IArticleCategoryValidatorService, ArticleCategoryValidatorService>();
+            services.AddTransient<IArticleApplication, ArticleApplication>();
+            services.AddTransient<IArticleRepository, ArticleRepository>();
             services.AddDbContext<MasterBloggerContext>(options =>
                 options.UseSqlServer(connectionString));
         }
