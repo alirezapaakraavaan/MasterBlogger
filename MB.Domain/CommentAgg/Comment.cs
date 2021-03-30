20cda8b654161f049560a52c2b739f3944278c3b
Alireza Pakravan <66172389+alirezapaakraavaan@users.noreply.github.com>
2021-03-30 17:13:49 +0430

    Framework implemented

--- MB.Domain/CommentAgg/Comment.cs
+++ MB.Domain/CommentAgg/Comment.cs
@@ -1,16 +1,15 @@
 ﻿using System;
+using _01_Framework.Domain;
 using MB.Domain.ArticleAgg;
 
 namespace MB.Domain.CommentAgg
 {
-    public class Comment
+    public class Comment : DomainBase<long>
     {
-        public long Id { get; private set; }
         public string Name { get; private set; }
         public string Email { get; private set; }
         public string Message { get; private set; }
         public int Status { get; private set; }
-        public DateTime CreationDate { get; private set; }
         public long ArticleId { get; private set; }
         public Article Article { get; private set; }
 
@@ -24,7 +23,6 @@ namespace MB.Domain.CommentAgg
             Email = email;
             Message = message;
             ArticleId = articleId;
-            CreationDate = DateTime.Now;
             Status = Statuses.New;
         }
 
